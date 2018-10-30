bf23f213dab1b7886e217c1f0d67d794d77e7235
nys <nays111@naver.com>
2018-10-31 00:29:56 +0900

    layout_1

---
+++ app/src/main/java/teampro/mju/yeogin_moreulkkeol/LoginActivity.java
@@ -0,0 +1,38 @@
+package teampro.mju.yeogin_moreulkkeol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.content.Intent;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText idText = (EditText) findViewById(R.id.idText);
+        EditText passwordText = (EditText) findViewById(R.id.passwordText);
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        TextView registerButton = (TextView) findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(intent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, Main2Activity.class);
+                LoginActivity.this.startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/teampro/mju/yeogin_moreulkkeol/SettingActivity.java
@@ -0,0 +1,42 @@
+package teampro.mju.yeogin_moreulkkeol;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        Button editProfileButton = (Button) findViewById(R.id.editProfileButton);
+        Button registerFavoriteButton = (Button) findViewById(R.id.registerFavoriteButton);
+        Button myFavoriteButton = (Button) findViewById(R.id.myFavoriteButton);
+        Button logoutButton = (Button) findViewById(R.id.logoutButton);
+
+        ImageButton backToMainButton = (ImageButton) findViewById(R.id.backToMainButton);
+
+        /*뒤로가기 버튼을 클릭하면 메인화면으로 돌아감.*/
+        backToMainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingActivity.this,Main2Activity.class);
+                SettingActivity.this.startActivity(intent);
+            }
+        });
+
+        //로그아웃 버튼을 클릭하면 로그인화면으로 돌아감*/
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingActivity.this,LoginActivity.class);
+                SettingActivity.this.startActivity(intent);
+            }
+        });
+    }
+}
